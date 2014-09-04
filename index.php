f1ec3bba38d1df42c29d0ba36e816d4fef1be14a
sbranly <shauna.branly@gmail.com>
2014-09-04 16:33:58 -0400

    dfsdf

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 function GetInfoLogData()
 {
-  $txt_file    = file_get_contents('info.log');
+  $logfile = '/usr/opsscripts/' . 'info.log';
+  $txt_file    = file_get_contents($logfile);
   $rows        = explode("\n", $txt_file);
   array_shift($rows);
 
-  $rows = array_slice($rows, -5);
+  //$rows = array_slice($rows, -5);
 
   $infologrow = '';
 
@@ -20,23 +21,39 @@ function GetInfoLogData()
           $info[$row]['opsviewpurge']         = $row_data[3];
           $info[$row]['opsviewadd']           = $row_data[4];
 
-          //display data
-          $infologrow .= "\n\t\t\t<tr>
-                    <td>". $info[$row]['lastrun']. "</td>
-                    <td>". $info[$row]['opsviewquery']. "</td>
-                    <td>". $info[$row]['awsquery']. "</td>
-                    <td>". $info[$row]['opsviewpurge']. "</td>
-                    <td>". $info[$row]['opsviewadd']. "</td>
-                    </td>";
+
+          if ($info[$row]['opsviewquery'] == "FAILED" || $info[$row]['awsquery'] == "FAILED" || $info[$row]['opsviewpurge'] == "FAILED" || $info[$row]['opsviewadd'] == "FAILED")
+          {
+            //display data
+            $infologrow .= "\n\t\t\t<tr class=\"danger\" >
+                <td>". $info[$row]['lastrun']. "</td>
+                <td>". $info[$row]['opsviewquery']. "</td>
+                <td>". $info[$row]['awsquery']. "</td>
+                <td>". $info[$row]['opsviewpurge']. "</td>
+                <td>". $info[$row]['opsviewadd']. "</td>
+                </td>";
+          }
+          else
+          {
+            //display data
+            $infologrow .= "\n\t\t\t<tr>
+                <td>". $info[$row]['lastrun']. "</td>
+                <td>". $info[$row]['opsviewquery']. "</td>
+                <td>". $info[$row]['awsquery']. "</td>
+                <td>". $info[$row]['opsviewpurge']. "</td>
+                <td>". $info[$row]['opsviewadd']. "</td>
+                </td>";
+          }
           $infologrow .= "\t\t\t</tr>";
     }
     return $infologrow;
   }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Opsview Services - Status</title>
+  <title>Opsview Services</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
@@ -48,7 +65,7 @@ function GetInfoLogData()
   <div class="container" id="update_config">
     <div class="panel panel-primary">
       <div class="panel-heading">
-        <h3 class="panel-title">Opsview Services - Status</h3>
+        <h3 class="panel-title">Opsview Services</h3>
       </div>
     </div>
     <div class="panel panel-info">
@@ -72,10 +89,19 @@ function GetInfoLogData()
       </table>
     </div>
     </div>
-    <div class="list-group">
-      <a href="/" class="list-group-item">Back</a>
+  </div>
+  <div class="container" id="config_options">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">Configuration</h3>
+      </div>
+    </div>
+    <div class="panel panel-info">
+      <div class="list-group">
+        <a href="http://opsviewservices.jpg.com/update/" class="list-group-item">Configure ELBs and Hostgroups</a>
+        <a href="http://opsviewservices.jpg.com/template/" class="list-group-item">Manage JSON Templates</a>
+      </div>
     </div>
   </div>
-
 </body>
 </html>
